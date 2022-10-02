62129058e40b597e8f0e474592275acc9e76b08e
L00162115 <L00162115.atu.ie>
2022-10-02 19:34:41 +0100

    Michael added HelloAtu.Java

---
+++ HelloAtu.java
@@ -0,0 +1,7 @@
+public class HelloATU {
+    public static void main(String[] args) {
+        // Prints "Hello, ATU from L00162115 Michael" as output
+        System.out.println("Hello, ATU LYIT from L00162115 Michael");
+    }
+}
+
